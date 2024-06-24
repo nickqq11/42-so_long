59cebb6f0f000b25f76541f3cdcbc029df61a579
nickqq11 <118785245+nickqq11@users.noreply.github.com>
2024-06-24 21:42:02 +0930

    doing

--- check.c
+++ check.c
@@ -1,18 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   check.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/04 15:06:16 by nhuang            #+#    #+#             */
-/*   Updated: 2024/06/24 21:00:25 by nhuang           ###   ########.fr       */
+/*   Created: 2024/06/24 20:58:40 by nhuang            #+#    #+#             */
+/*   Updated: 2024/06/24 20:58:42 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "so_long.h"
-#include "minilibx/mlx.h"
-#include <stdio.h>
-
-// // gcc so_long.c -o a -I./minilibx -L./minilibx -lmlx -framework OpenGL -framework AppKit  
 
--- move.c
+++ move.c
@@ -1,18 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   move.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/04 15:06:16 by nhuang            #+#    #+#             */
-/*   Updated: 2024/06/24 21:00:25 by nhuang           ###   ########.fr       */
+/*   Created: 2024/06/24 20:58:56 by nhuang            #+#    #+#             */
+/*   Updated: 2024/06/24 20:58:57 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "so_long.h"
-#include "minilibx/mlx.h"
-#include <stdio.h>
-
-// // gcc so_long.c -o a -I./minilibx -L./minilibx -lmlx -framework OpenGL -framework AppKit  
 
