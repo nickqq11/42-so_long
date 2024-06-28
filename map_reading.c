0d879646804b8aeca6828d13e90ac79b825e2364
nickqq11 <nickaids@gmail.com>
2024-06-28 17:53:33 +0930

    working on

--- map_reading.c
+++ map_reading.c
@@ -6,7 +6,7 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 20:57:54 by nhuang            #+#    #+#             */
-/*   Updated: 2024/06/28 16:49:38 by nhuang           ###   ########.fr       */
+/*   Updated: 2024/06/28 17:53:21 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,15 @@ char **read_map(t_game *game, char **argv)
 	game->fd = open(argv[1], O_RDONLY);
 	if (!game->fd)
 		ft_printf("fd not opening.\n");
-	
+	map = malloc((game->height + 1) * sizeof(char *) );
+	if (!map)
+		return(ft_printf("malloc error.\n"));
+	i = -1;
+	while(++i < game->height)
+	{
+		map[i] = get_next_line(game->fd);
+		
+	}
 }
 
 int map_load(t_game *game, char **argv)
